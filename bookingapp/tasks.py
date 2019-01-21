ea53827c3e757c8ae372f0c1612e687fdec2a5c1
Robert Njane <robert.njane@andela.com>
2019-01-21 13:22:55 +0300

    Update Email Send

--- bookingapp/tasks.py
+++ bookingapp/tasks.py
@@ -12,17 +12,17 @@ def flight_reminder():
     flight_bookings = models.FlightBooking.objects.all()
     for flight_booking in flight_bookings:
         flight_time = flight_booking.flight.date_time_of_flight
-        # if datetime.now(flight_time.tzinfo) + timedelta(hours=24) >= flight_time >= datetime.now(flight_time.tzinfo) + timedelta(hours=23, minute=30):
-        send_mail(
-        'Reminder: Your upcoming flight',
-        flight_booking.flight.name + "Will be on" + flight_booking.flight.date_time_of_flight.strftime("%Y-%m-%d %H:%M:%S"),
-        settings.EMAIL_HOST_USER,
-        [flight_booking.owner.email, 'robert.ndungu@hotmail.com'],
-        fail_silently=True,
-        )
+        if datetime.now(flight_time.tzinfo) + timedelta(hours=24) >= flight_time >= datetime.now(flight_time.tzinfo) + timedelta(hours=23, minute=30):
+            send_mail(
+            'Reminder: Your upcoming flight',
+            flight_booking.flight.name + "Will be on" + flight_booking.flight.date_time_of_flight.strftime("%Y-%m-%d %H:%M:%S"),
+            settings.EMAIL_HOST_USER,
+            [flight_booking.owner.email, 'robert.ndungu@hotmail.com'],
+            fail_silently=True,
+            )
 
 @periodic_task(
-    run_every=(crontab(minute='*/1')),
+    run_every=(crontab(minute='*/60')),
     name="send_reminder_email",
     ignore_result=True
 )
