872b8b65c5fe78eb6e37a8c54dae4ef5c5698914
Robert Njane <robert.njane@andela.com>
2019-01-21 12:37:02 +0300

    update

--- bookingapp/urls.py
+++ bookingapp/urls.py
@@ -5,6 +5,7 @@ from . import views
 from django.conf.urls import url, include
 
 urlpatterns = [
+    path('', views.ViewBookings.as_view(), name='view_bookings'),
     path('api-auth/', include('rest_framework.urls')),
     path('login/', views.LoginUser.as_view(), name='login'),
     path('register/', views.UserCreate.as_view(), name='register'),
